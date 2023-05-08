bdaaffb02c2d05661310bc51a935e5d95d149070
Kevin Vetter <kevinmvetter@gmail.com>
2023-05-08 12:23:05 +0200

    fin - ish

---
+++ BlazorWebShop/Model/Link.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorWebShop.Model
+{
+    public class Link
+    {
+        public string PageText { get; set; }
+        public int PageValue { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsDisabled { get; set; }
+    }
+}
---
+++ BlazorWebShop/Model/Parameters.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorWebShop.Model
+{
+    public class Parameters
+    {
+        public int CurrentPage { get; set; } = 1;
+        public int TotalPages { get; set; }
+        public int PageSize { get; set; } = 25;
+        public int TotalCount { get; set; }
+    }
+}
